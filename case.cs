137216d2b87c80730a926433c4bf87bf814a7676
Abhinav Rajaseshan <abhinavseshan@hotmail.com>
2016-01-17 00:42:20 -0500

    update case

--- case.cs
+++ case.cs
@@ -4,25 +4,24 @@ string data[] = received.split(",", null);
 switch (data[0]){
 	case "values":
 		if (data[1] == "P1"){
-			player1.rotation = new Vector3(data[5], data[6], data[7]);
-			player1.postion = new Vector3(data[2], data[3], data[4]);
+			player1.rotation = new Vector3(Float.tryParse(data[5]), Float.tryParse(data[6]), Float.tryParse(data[7]));
+			player1.postion = new Vector3(Float.tryParse(data[2]), Float.tryParse(data[3]), Float.tryParse(data[4]));
 		}
 		
 		if (data[1] == "P2"){
-			player2.rotation = new Vector3(data[5], data[6], data[7]);
-			player2.postion = new Vector3(data[2], data[3], data[4]);
-
+			player2.rotation = new Vector3(Float.tryParse(data[5]), Float.tryParse(data[6]), Float.tryParse(data[7]));
+			player2.postion = new Vector3(Float.tryParse(data[2]), Float.tryParse(data[3]), Float.tryParse(data[4]));
 		}
 		
 		break;
 	
 	case "saber":
 		if (data[1] == "P1"){
-			player1.lightSaberOn = (bool) data[2];
+			player1.lightSaberOn = Boolean.tryParse(data[2]);
 		}
 		
 		if (data[1] == "P2"){
-			player2.lightSaberOn = (bool) data[2];
+			player2.lightSaberOn = Boolean.tryParse(data[2]);
 		}
 		
 		break;
@@ -40,11 +39,11 @@ switch (data[0]){
 		
 	case "saberColor":
 		if (data[1] == "P1"){
-			player1.setSaberColor = data[2];
+			player1.setSaberColor = Integer.tryParse(data[2]);
 		}
 		
 		if (data[1] == "P2"){
-			player2.setSaberColor = data[3];
+			player2.setSaberColor = Integer.tryParse(data[3]);
 		}
 		
 		break;
